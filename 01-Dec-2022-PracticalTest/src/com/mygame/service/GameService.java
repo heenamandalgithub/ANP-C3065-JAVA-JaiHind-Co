6ed97544fce4759f2e3fd75d3ff4ee172976b5c8
Heena Mandal <99394465+heenamandalgithub@users.noreply.github.com>
2022-12-02 18:11:37 +0530

    Update GameService.java

--- 01-Dec-2022-PracticalTest/src/com/mygame/service/GameService.java
+++ 01-Dec-2022-PracticalTest/src/com/mygame/service/GameService.java
@@ -2,9 +2,11 @@ package com.mygame.service;
 import com.mygame.dao.GameDAO;
 
 public class GameService {
-public void getcityNameService()
+public String[]  getcityNameService()
 {
 	GameDAO GameDAOobj=new GameDAO();
 	GameDAOobj.getcityName();
+	String s[]=GameDAOobj.getcityName();
+	return s;
 }
 }
