3f905071cb25e972080b62b6f2d1787d5bdd9c0f
Heena Mandal <99394465+heenamandalgithub@users.noreply.github.com>
2022-12-02 15:14:20 +0530

    Update GameView.java

--- 01-Dec-2022-PracticalTest/src/com/mygame/view/GameView.java
+++ 01-Dec-2022-PracticalTest/src/com/mygame/view/GameView.java
@@ -1,22 +1,10 @@
 package com.mygame.view;
 
+
 import com.mygame.service.*;
 import java.util.Random.*;
 
 public class GameView {
-	GameService gaming=new GameService();
-	String[] displaycityNameWithBlanks()
-	{
-		String[] s =gaming.getcityNameService();
-		for(int i=0;i<s.length;i++)
-		{
-			char arr[]=s[i].toCharArray();
-			
-			for(int j=0;i<arr.length;j++)
-			{
-				 arr[i]=(char) (arr.length*Math.random());	
-		}
-	}
-		return s;
+	
 	}
 }
