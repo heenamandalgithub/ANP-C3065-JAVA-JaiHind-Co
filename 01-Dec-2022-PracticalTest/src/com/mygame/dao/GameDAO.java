3de32a07ae96ffca54a465d97e8447c78f611a8d
Heena Mandal <99394465+heenamandalgithub@users.noreply.github.com>
2022-12-02 18:09:18 +0530

    Update GameDAO.java

--- 01-Dec-2022-PracticalTest/src/com/mygame/dao/GameDAO.java
+++ 01-Dec-2022-PracticalTest/src/com/mygame/dao/GameDAO.java
@@ -3,10 +3,7 @@ package com.mygame.dao;
 public class GameDAO {
   static String cityName[]={"Chennai","Mumbai","Delhi","Kolkata","Hyderabad"};
    
-public void getcityName() {
-	for(int i=0;i<cityName.length;i++) {
-		System.out.println(cityName[i]);
-	}
-	
+public String[]getcityName() {
+	return cityName;
 }
 }
