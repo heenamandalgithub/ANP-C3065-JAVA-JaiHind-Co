b0c187b9c88e901c6d6a8281c9eb4e53c10d55ac
Heena Mandal <99394465+heenamandalgithub@users.noreply.github.com>
2022-12-10 12:41:56 +0530

    version 1

--- 08_Dec_2022_Practical_Test/src/com/mymart/view/ProductView.java
+++ 08_Dec_2022_Practical_Test/src/com/mymart/view/ProductView.java
@@ -5,7 +5,15 @@ package com.mymart.view;
 	import com.mymart.service.ProductService;
 
 	public class ProductView {
+		private static ProductService productService=new ProductService();
 		
+		public void addProduct()
+			//Scanner obj,write to accept input user,store in local var;
+			int pId=1;
+			String pName="Dell Laptop";
+			double pPrice=75000;
+		Product product=new Product(pId,pName,pPrice);
+		productService.addProductService(Product);
 
 	}
 
