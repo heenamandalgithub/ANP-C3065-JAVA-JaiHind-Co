2981dcea9a059a4788c7715de7288fa63064d798
Heena Mandal <99394465+heenamandalgithub@users.noreply.github.com>
2022-12-10 12:35:36 +0530

    Update Main.java

--- 08_Dec_2022_Practical_Test/src/com/mymart/main/Main.java
+++ 08_Dec_2022_Practical_Test/src/com/mymart/main/Main.java
@@ -1,5 +1,11 @@
 package com.mymart.main;
 
 public class Main {
-
+	public static void main(String[] args) {
+		ProductView productView=new ProductView();
+		productView.addProduct();
+		ProductView.displayAllProducts();
+	}
 }
+
+
