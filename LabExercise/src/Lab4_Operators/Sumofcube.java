b7e42c876d4a461e27926ddc9600e6486cf3aeba
Heena Mandal <99394465+heenamandalgithub@users.noreply.github.com>
2022-12-15 21:37:53 +0530

    version 1

--- LabExercise/src/Lab4_Operators/Sumofcube.java
+++ LabExercise/src/Lab4_Operators/Sumofcube.java
@@ -7,16 +7,13 @@ public class Sumofcube {
 	public int calculateCube(int n) {
 		
 	while(n>0) {
-// here we are storing  a remainder value inside variable ' a ' because we want the individual number so that we can calculate cube of that number.			
 	int a=n%10;
 			
-//here the sum of cubes of each individual digit get stored in the cube . 
 	cube=cube+(a*a*a);
 			
-// here we are dividing the number by 10 , so that after calculating the cube of the individual number then that number will became zero and will not be further repeated .
 	n=n/10;
 				
-		}
+	}
 		
 	System.out.println("The Sum Of Cubes Of Entered Number Is : ");
 		
