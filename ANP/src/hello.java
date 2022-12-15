7c4c94a813d2edc5310041bea1328233d5e614f9
Heena Mandal <99394465+heenamandalgithub@users.noreply.github.com>
2022-12-15 16:49:27 +0530

    Update hello.java

--- ANP/src/hello.java
+++ ANP/src/hello.java
@@ -4,7 +4,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hii ,how r u");
-System.out.println("hii ,how r u");
+System.out.println("Hello Welcome to my repository");
 	}
 
 }
